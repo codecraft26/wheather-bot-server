e9a03658029e007fb296c3dc5533a61490be323f
codecraft26 <gaman0221@gmial.com>
2024-01-20 19:28:31 +0530

    create auth using google auth

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -11,24 +11,29 @@ export class UserService {
     constructor(
         @InjectModel(User.name)
         private userModel: Model<User>,
-        @InjectModel(Subscribed.name) private subscribedModel: Model<Subscribed> 
+        @InjectModel(Subscribed.name) private subscribedModel: Model<Subscribed>
     ) {
 
     }
 
 
+    googleLogin(req) {
+        if (!req.user) {
+            return 'No user from google'
+        }
+
+        return {
+            message: 'User information from google',
+            user: req.user
+        }
+    }
+
+
+    
+
+
     async findOrCreateUser(chatId: number, userName: string): Promise<User> {
-
-
-
-
         const user = await this.userModel.findOne({ chatId })
-
-
-
-
-
-
         if (!user) {
             const user = await this.userModel.create({
                 chatId: chatId,
@@ -49,7 +54,7 @@ export class UserService {
 
 
         if (user) {
-               
+
 
 
             return user;
@@ -66,62 +71,62 @@ export class UserService {
                 await user.save();
             }
         } catch (error) {
-           throw new Error(`User with chatId ${chatId} not found`);
+            throw new Error(`User with chatId ${chatId} not found`);
         }
-}
-async getUserLocation(chatId: number): Promise<string> {
-    try {
-        const user = await this.userModel.findOne({ chatId });
-        return user ? user.location : null;
-    } catch (error) {
-        
-       throw new Error(`User with chatId ${chatId} not found`);
     }
-}
+    async getUserLocation(chatId: number): Promise<string> {
+        try {
+            const user = await this.userModel.findOne({ chatId });
+            return user ? user.location : null;
+        } catch (error) {
 
-
-async getUser(id: string): Promise<User> {
-    try {
-        const user = await this.userModel.findById(id);
-        return user;
-    } catch (error) {
-        
-       throw new Error(`User with chatId `);
-    }
-}
-
-async addToSubscribedModel(chatId: number): Promise<void> {
-    // Find the user by their chatId
-    const user = await this.userModel.findOne({ chatId: chatId }).exec();
-    if (!user) {
-        throw new Error('User not found');
+            throw new Error(`User with chatId ${chatId} not found`);
+        }
     }
 
-    
-    const isSubscribed = await this.subscribedModel.findOne({ users: user._id }).exec();
-    if (isSubscribed) {
-        throw new Error('User already subscribed');
+
+    async getUser(id: string): Promise<User> {
+        try {
+            const user = await this.userModel.findById(id);
+            return user;
+        } catch (error) {
+
+            throw new Error(`User with chatId `);
+        }
     }
 
-    const subscribed = new this.subscribedModel({
-        users: [user._id],
-    });
-
-    await subscribed.save();
-}
-
-async checkIfUserSubscribed(chatId: number): Promise<boolean> {
-    
-    const subscribedUser = await this.subscribedModel.findOne({ chatId: chatId }).exec();
-    return !!subscribedUser;
-}
-
-async getSubscribedUsers():Promise<User[]> {
-    const subscribedUsers = await this.subscribedModel.find().populate('users').exec();
-   return subscribedUsers.map((subscribed) => subscribed.users).flat();
+    async addToSubscribedModel(chatId: number): Promise<void> {
+        // Find the user by their chatId
+        const user = await this.userModel.findOne({ chatId: chatId }).exec();
+        if (!user) {
+            throw new Error('User not found');
+        }
 
 
-}
+        const isSubscribed = await this.subscribedModel.findOne({ users: user._id }).exec();
+        if (isSubscribed) {
+            throw new Error('User already subscribed');
+        }
+
+        const subscribed = new this.subscribedModel({
+            users: [user._id],
+        });
+
+        await subscribed.save();
+    }
+
+    async checkIfUserSubscribed(chatId: number): Promise<boolean> {
+
+        const subscribedUser = await this.subscribedModel.findOne({ chatId: chatId }).exec();
+        return !!subscribedUser;
+    }
+
+    async getSubscribedUsers(): Promise<User[]> {
+        const subscribedUsers = await this.subscribedModel.find().populate('users').exec();
+        return subscribedUsers.map((subscribed) => subscribed.users).flat();
+
+
+    }
 
 
 
